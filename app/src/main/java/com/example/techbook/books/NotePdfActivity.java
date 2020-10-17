1597e609683dfeb9447d9bca1d1edfd5b1808364
Narendra <narendra.bhatt@cclsolution.com>
2020-10-17 12:21:18 +0545

    added toolbar in note pdf  activity

--- app/src/main/java/com/example/techbook/books/NotePdfActivity.java
+++ app/src/main/java/com/example/techbook/books/NotePdfActivity.java
@@ -2,18 +2,20 @@ package com.example.techbook.books;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.techbook.R;
 import com.github.barteksc.pdfviewer.PDFView;
 
-import java.util.Objects;
+import java.util.Locale;
 
 public class NotePdfActivity extends AppCompatActivity {
 
     String bookName;
     PDFView pdfView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +23,11 @@ public class NotePdfActivity extends AppCompatActivity {
         setContentView(R.layout.activity_note_pdf);
 
         pdfView = findViewById(R.id.pdfView);
+        toolbar = findViewById(R.id.pdfToolbar)
 
         bookName = getIntent().getExtras().getString(getString(R.string.book_name));
         Log.d("BOOKNAME", bookName);
+        toolbar.setTitle(bookName.toUpperCase(Locale.ROOT));
         pdfView.fromAsset(bookName + ".pdf").load();
 
     }
